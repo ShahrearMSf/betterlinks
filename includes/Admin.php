d4bcf2a6d5c1145fa1746cc8976680571af49d4d
Tushar Imran <tusharimran9909@gmail.com>
2020-12-24 18:15:39 +0600

    remove others plugin scripts from our plugin settings page

--- includes/Admin.php
+++ includes/Admin.php
@@ -6,11 +6,19 @@ class Admin {
     {
         $this->add_menu();
         $this->add_scripts();
+        add_filter('BetterLinks/Admin/skip_no_conflict', array($this, 'skip_no_conflict'));
     }
     public function add_menu(){
         new Admin\Menu();
     }
     public function add_scripts(){
         new Admin\Assets();
+    }
+    public function skip_no_conflict(){
+        $whitelist = array('127.0.0.1', "::1");
+        if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+            return true;
+        }
+        return false;
     }
 }
--- includes/Admin/Assets.php
+++ includes/Admin/Assets.php
@@ -19,6 +19,27 @@ class Assets
     public function plugin_scripts($hook)
     {
         if (\BetterLinks\Helper::plugin_page_hook_suffix($hook)) {
+            add_action('wp_print_scripts', function () {
+                $isSkip = apply_filters('BetterLinks/Admin/skip_no_conflict', false);
+    
+                if ($isSkip) {
+                    return;
+                }
+    
+                global $wp_scripts;
+                if (!$wp_scripts) {
+                    return;
+                }
+    
+                $pluginUrl = plugins_url();
+                foreach ($wp_scripts->queue as $script) {
+                    $src = $wp_scripts->registered[$script]->src;
+                    if (strpos($src, $pluginUrl) !== false && !strpos($src, BL_PLUGIN_SLUG) !== false) {
+                        wp_dequeue_script($wp_scripts->registered[$script]->handle);
+                    }
+                }
+            }, 1);
+
             wp_enqueue_style('betterlinks-admin-style', BL_ASSETS_URI . 'css/admin.css', array(), filemtime(BL_ASSETS_DIR_PATH . 'css/admin.css'), 'all');
             // js
             wp_enqueue_script('betterlinks-admin-scripts', BL_ASSETS_URI . 'js/admin.js', array('jquery'), filemtime(BL_ASSETS_DIR_PATH . 'js/admin.js'), false);
