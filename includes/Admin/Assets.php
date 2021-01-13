cb58fbc057480c63b1ab5a562918a0d6540621bd
Tushar Imran <tusharimran9909@gmail.com>
2021-01-13 12:23:41 +0600

    update assets version

--- includes/Admin/Assets.php
+++ includes/Admin/Assets.php
@@ -40,6 +40,7 @@ class Assets
                 }
             }, 1);
 
+            wp_enqueue_style('betterlinks-chartjs', BETTERLINKS_ASSETS_URI . 'css/lib/Chart.min.css', array(), filemtime(BETTERLINKS_ASSETS_DIR_PATH . 'css/lib/Chart.min.css'), 'all');
             wp_enqueue_style('betterlinks-admin-style', BETTERLINKS_ASSETS_URI . 'css/betterlinks.css', array(), filemtime(BETTERLINKS_ASSETS_DIR_PATH . 'css/betterlinks.css'), 'all');
             // js
             wp_enqueue_script('betterlinks-moment', BETTERLINKS_ASSETS_URI . 'js/lib/moment.min.js', array('jquery'), filemtime(BETTERLINKS_ASSETS_DIR_PATH . 'js/lib/moment.min.js'), true);
