7866874388c343209938cf142d7e786c67eeb95d
Tushar Imran <tusharimran9909@gmail.com>
2021-01-20 11:59:04 +0600

    added data exporter

--- includes/Tools/Export.php
+++ includes/Tools/Export.php
@@ -5,18 +5,49 @@ use Apfelbox\FileDownload\FileDownload;
 
 class Export 
 {
+    private $wpdb_prefix;
+    private $DB;
 	public function __construct()
 	{
-		add_action( 'wp_ajax_betterlinks/tools/export_data', array($this, 'export_data') );
+		add_action( 'admin_init', array($this, 'export_data') );
 	}
-	function export_data() {
+	public function export_data() {
+        $page = (isset($_GET['page']) ? $_GET['page'] : '');
+        $export = (isset($_GET['export']) ? $_GET['export'] : false);
+        if( $page === 'betterlinks-settings' && $export == true){
+            $content = $this->process_data((isset($_POST['content']) ? $_POST['content'] : ''));
+            $fileDownload = FileDownload::createFromString(json_encode($content));
+            $filename = 'betterlinks.' . date('Y-m-d H:i:s') . '.json';
+            $fileDownload->sendDownload($filename);
+        }
+    }
+    public function process_data($type){
+        global $wpdb;
+		$this->wpdb_prefix = $wpdb->prefix;
+        $this->DB = \BetterLinks\Helper::DB();
+        $content = [];
+		if($type == 'all'){
+            $content['links'] = $this->get_links();
+            $content['terms'] = $this->get_terms();
+            $content['terms_relationships'] = $this->get_terms_relationships();
+            $content['clicks'] = $this->get_clicks();
+        } else if($type == 'clicks'){
+            $content['clicks'] = $this->get_clicks();
+        }
+		return $content;
+    }
 
-        
-        $content = "This is the content of the file:";
-        $fileDownload = FileDownload::createFromString($content);
-        $fileDownload->sendDownload("download.txt");
-        
 
-        wp_die(); // this is required to terminate immediately and return a proper response
+    public function get_links(){
+        return $this->DB->query("SELECT * from {$this->wpdb_prefix}betterlinks")->get();
+    }
+    public function get_clicks(){
+        return $this->DB->query("SELECT * from {$this->wpdb_prefix}betterlinks_clicks")->get();
+    }
+    public function get_terms(){
+        return $this->DB->query("SELECT * from {$this->wpdb_prefix}betterlinks_terms")->get();
+    }
+    public function get_terms_relationships(){
+        return $this->DB->query("SELECT * from {$this->wpdb_prefix}betterlinks_terms_relationships")->get();
     }
 }
--- src/containers/group/TabsTools.js
+++ src/containers/group/TabsTools.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-function useQuery() {
-	return new URLSearchParams(useLocation().search);
-}
 
-const TabsTools = () => {
-	let query = useQuery();
+const TabsTools = ({ query }) => {
 	return (
 		<React.Fragment>
 			<div>
 				<h3>Choose what to export</h3>
-
-				<form action={'admin.php?page=' + query.get('page')} method="POST">
+				<form action={'admin.php?page=' + query.get('page') + '&export=true'} method="POST">
 					<div role="group" aria-labelledby="my-radio-group">
 						<div>
 							<label>
@@ -28,7 +22,7 @@ const TabsTools = () => {
 						</div>
 						<div>
 							<label>
-								<input type="radio" name="content" value="analytic" />
+								<input type="radio" name="content" value="clicks" />
 								Analytic
 							</label>
 						</div>
