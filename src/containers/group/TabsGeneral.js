d9ef9c14f9692434cbd0376f5a143ebeb1bd0f48
Sapan Mozammel <sapanmozammel@gmail.com>
2021-01-20 13:44:06 +0600

    setting field design

--- src/containers/group/TabsGeneral.js
+++ src/containers/group/TabsGeneral.js
@@ -2,7 +2,29 @@ import React from 'react';
 const TabsGeneral = () => {
 	return (
 		<React.Fragment>
-			<div>This is general tabs</div>
+			<span className='btl-form-group'>
+				<label className="btl-form-label">Hello world</label>
+				<input type="text" className="btl-form-control" />
+			</span>
+			<span className='btl-form-group'>
+				<label className="btl-form-label">Hello another world</label>
+				<input type="text" className="btl-form-control" placeholder="hello world" />
+			</span>
+			<span className='btl-form-group'>
+				<label className="btl-form-label">Hello world</label>
+				<input type="text" className="btl-form-control" placeholder="Hello another world"/>
+			</span>
+			<span className='btl-form-group'>
+				<label className="btl-form-label">Hello world</label>
+				<textarea type="text" className="btl-form-control" placeholder="message"></textarea>
+			</span>
+			<span className='btl-form-group'>
+				<label className="btl-form-label">Hello world</label>
+				<label className="btl-checkbox-field">
+					<input name="nofollow" className="btl-check" type="checkbox" value="false" />
+						<span className="text">No Follow</span>
+				</label>
+			</span>
 		</React.Fragment>
 	);
 };
