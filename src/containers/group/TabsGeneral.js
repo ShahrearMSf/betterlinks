222255ccf1ffcbe97fb0e73ae7ba28683576817a
Sapan Mozammel <sapanmozammel@gmail.com>
2021-01-26 18:38:04 +0600

    markup

--- src/containers/group/TabsGeneral.js
+++ src/containers/group/TabsGeneral.js
@@ -2,29 +2,31 @@ import React from 'react';
 const TabsGeneral = () => {
 	return (
 		<React.Fragment>
-			<span className='btl-form-group'>
-				<label className="btl-form-label">Hello world</label>
-				<input type="text" className="btl-form-control" />
-			</span>
-			<span className='btl-form-group'>
-				<label className="btl-form-label">Hello another world</label>
-				<input type="text" className="btl-form-control" placeholder="hello world" />
-			</span>
-			<span className='btl-form-group'>
-				<label className="btl-form-label">Hello world</label>
-				<input type="text" className="btl-form-control" placeholder="Hello another world"/>
-			</span>
-			<span className='btl-form-group'>
-				<label className="btl-form-label">Hello world</label>
-				<textarea type="text" className="btl-form-control" placeholder="message"></textarea>
-			</span>
-			<span className='btl-form-group'>
-				<label className="btl-form-label">Hello world</label>
-				<label className="btl-checkbox-field">
-					<input name="nofollow" className="btl-check" type="checkbox" value="false" />
-						<span className="text">No Follow</span>
-				</label>
-			</span>
+			<div className="btl-tab-panel-inner">
+				<span className='btl-form-group'>
+					<label className="btl-form-label">Hello world</label>
+					<input type="text" className="btl-form-control" />
+				</span>
+				<span className='btl-form-group'>
+					<label className="btl-form-label">Hello another world</label>
+					<input type="text" className="btl-form-control" placeholder="hello world" />
+				</span>
+				<span className='btl-form-group'>
+					<label className="btl-form-label">Hello world</label>
+					<input type="text" className="btl-form-control" placeholder="Hello another world"/>
+				</span>
+				<span className='btl-form-group'>
+					<label className="btl-form-label">Hello world</label>
+					<textarea type="text" className="btl-form-control" placeholder="message"></textarea>
+				</span>
+				<span className='btl-form-group'>
+					<label className="btl-form-label">Hello world</label>
+					<label className="btl-checkbox-field">
+						<input name="nofollow" className="btl-check" type="checkbox" value="false" />
+							<span className="text">No Follow</span>
+					</label>
+				</span>
+			</div>
 		</React.Fragment>
 	);
 };
--- src/containers/group/TabsTools.js
+++ src/containers/group/TabsTools.js
@@ -3,47 +3,49 @@ import { Formik, Field, Form } from 'formik';
 const TabsTools = () => {
 	return (
 		<React.Fragment>
-			<div>
-				<h3>Choose what to export</h3>
-				<Formik
-					initialValues={{
-						content: 'all',
-					}}
-					onSubmit={(values) => {
-						console.log(values);
-					}}
-				>
-					<Form>
-						<div role="group" aria-labelledby="my-radio-group">
-							<div>
-								<label>
-									<Field type="radio" name="content" value="all" />
-									All content
-								</label>
-								<span>This will contain all of your links, analytic and settings.</span>
+			<div className="btl-tab-inner-divider">
+				<div className="btl-tab-panel-inner">
+					<h3>Choose what to export</h3>
+					<Formik
+						initialValues={{
+							content: 'all',
+						}}
+						onSubmit={(values) => {
+							console.log(values);
+						}}
+					>
+						<Form>
+							<div role="group" aria-labelledby="my-radio-group">
+								<div>
+									<label>
+										<Field type="radio" name="content" value="all" />
+										All content
+									</label>
+									<span>This will contain all of your links, analytic and settings.</span>
+								</div>
+								<div>
+									<label>
+										<Field type="radio" name="content" value="links" />
+										Manage Links
+									</label>
+								</div>
+								<div>
+									<label>
+										<Field type="radio" name="content" value="analytic" />
+										Analytic
+									</label>
+								</div>
+								<div>
+									<label>
+										<Field type="radio" name="content" value="settings" />
+										Settings
+									</label>
+								</div>
 							</div>
-							<div>
-								<label>
-									<Field type="radio" name="content" value="links" />
-									Manage Links
-								</label>
-							</div>
-							<div>
-								<label>
-									<Field type="radio" name="content" value="analytic" />
-									Analytic
-								</label>
-							</div>
-							<div>
-								<label>
-									<Field type="radio" name="content" value="settings" />
-									Settings
-								</label>
-							</div>
-						</div>
-						<button type="submit">Download Export File</button>
-					</Form>
-				</Formik>
+							<button type="submit">Download Export File</button>
+						</Form>
+					</Formik>
+				</div>
 			</div>
 		</React.Fragment>
 	);
