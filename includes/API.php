1b139f4787190e8df2bd074e1e0a4f066070d546
Tushar Imran <tusharimran9909@gmail.com>
2021-01-31 11:53:39 +0600

    bug fix - jwt compatible api whitelist issue

--- includes/API.php
+++ includes/API.php
@@ -4,6 +4,7 @@ class API
 {
 	public function __construct()
 	{
+		$this->dispatch_hook();
 		$this->load_links_rest_API();
 		$this->load_terms_rest_API();
 		$this->load_clicks_rest_API();
@@ -20,4 +21,12 @@ class API
 	{
 		new API\Clicks();
 	}
+	public function dispatch_hook(){
+		add_filter('jwt_auth_whitelist', array($this, 'whitelist_API'));
+	}
+	public function whitelist_API($endpoints)
+    {
+        $endpoints[] = '/wp-json/' . BETTERLINKS_PLUGIN_SLUG . '/v1/*';
+        return $endpoints;
+    }
 }
