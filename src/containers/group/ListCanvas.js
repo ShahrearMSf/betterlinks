d8e8b43db2827f88e7053cb2d78042c06e923671
Tushar Imran <tusharimran9909@gmail.com>
2021-02-03 11:36:31 +0600

    added list view filter markup

--- src/containers/group/ListCanvas.js
+++ src/containers/group/ListCanvas.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import DataTable from 'react-data-table-component';
-import { fetch_links_data, onDragEnd, add_new_cat, add_new_link, edit_link, delete_link } from './../../redux/actions/links.actions';
+import Select from 'react-select';
+import { fetch_links_data, add_new_cat, add_new_link, edit_link, delete_link } from './../../redux/actions/links.actions';
 import CreateCategory from './../../components/CreateCategory';
 import Link from './../../components/Link';
 import CatHeader from './../../components/CatHeader';
@@ -61,16 +62,78 @@ const columns = [
 	},
 ];
 
+const FilterComponent = ({ filterText, onFilter, onClear }) => (
+	<React.Fragment>
+		<div className="btl-links-filter">
+			<div className="btl-click-filter">
+				<input id="search" type="text" placeholder={__('Search short link', 'betterlinks')} value={filterText} onChange={onFilter} />
+			</div>
+			<Select
+				options={[
+					{ value: '', label: 'Categories' },
+					{ value: 'shop', label: 'shop' },
+				]}
+			/>
+			<Select
+				options={[
+					{ value: '', label: 'Short by Clicks' },
+					{ value: 'unique', label: 'Unique Clicks' },
+					{ value: 'clicks', label: 'All Clicks' },
+				]}
+			/>
+			<Select
+				options={[
+					{ value: '', label: 'All Dates' },
+					{ value: 'Jan 2021', label: 'Unique Clicks' },
+				]}
+			/>
+			<input placeholder="Links to Show" />
+			<button>Filter</button>
+		</div>
+		<div className="btl-bulk-actions">
+			<Select
+				options={[
+					{ value: '', label: 'Bulk Actions' },
+					{ value: 'delete', label: 'Delete' },
+				]}
+			/>
+			<button>Apply</button>
+		</div>
+	</React.Fragment>
+);
+
 const ListCanvas = (props) => {
 	const { links } = props.links;
+	const [filterText, setFilterText] = useState('');
+	const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
 	var stored = Object.values(links).reduce(function (total, item) {
 		total = [...total, ...item.lists];
 		return total;
 	}, []);
+
+	const subHeaderComponentMemo = React.useMemo(() => {
+		const handleClear = () => {
+			if (filterText) {
+				setResetPaginationToggle(!resetPaginationToggle);
+				setFilterText('');
+			}
+		};
+		return <FilterComponent onFilter={(e) => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />;
+	}, [filterText, resetPaginationToggle]);
+
 	return (
 		<React.Fragment>
 			<div>
-				<DataTable className="btl-analytic-table" columns={columns} data={stored} pagination persistTableHead />
+				<DataTable
+					className="btl-analytic-table"
+					columns={columns}
+					data={stored.filter((item) => item.link_title && item.link_title.toLowerCase().includes(filterText.toLowerCase()))}
+					pagination
+					paginationResetDefaultPage={resetPaginationToggle}
+					subHeader
+					subHeaderComponent={subHeaderComponentMemo}
+					persistTableHead
+				/>
 			</div>
 		</React.Fragment>
 	);
