7c10bec5c44177f1cc6d3431fd58c9b1c993e691
Tushar Imran <tusharimran9909@gmail.com>
2021-02-09 15:53:04 +0600

    improve -  most recent links showing by default

--- includes/API/Links.php
+++ includes/API/Links.php
@@ -121,9 +121,8 @@ class Links extends Controller
         FROM {$prefix}betterlinks_terms
         LEFT JOIN  {$prefix}betterlinks_terms_relationships ON {$prefix}betterlinks_terms.ID = {$prefix}betterlinks_terms_relationships.term_id
         LEFT JOIN  {$prefix}betterlinks ON {$prefix}betterlinks.ID = {$prefix}betterlinks_terms_relationships.link_id
-        WHERE {$prefix}betterlinks_terms.term_type = 'category'"
-			)
-			->get();
+        WHERE {$prefix}betterlinks_terms.term_type = 'category' ORDER BY {$prefix}betterlinks.link_date DESC"
+			)->get();
 
 		return new \WP_REST_Response(
 			[
