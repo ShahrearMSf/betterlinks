9b4f31129e9576e5b623b48a81278a45cb04348e
Tushar Imran <tusharimran9909@gmail.com>
2021-02-24 11:11:42 +0600

    improve - analytic data showing order change and direct conflict fix with prettylinks

--- includes/API/Clicks.php
+++ includes/API/Clicks.php
@@ -77,7 +77,7 @@ class Clicks extends Controller
 				"SELECT CLICKS.ID as 
         click_ID, link_id, browser, created_at, referer, short_url, target_url, ip, {$prefix}betterlinks.link_title,
         (select count(id) from {$prefix}betterlinks_clicks where CLICKS.ip = {$prefix}betterlinks_clicks.ip group by ip) as IPCOUNT
-		from {$prefix}betterlinks_clicks as CLICKS left join {$prefix}betterlinks on {$prefix}betterlinks.id = CLICKS.link_id WHERE created_at BETWEEN '{$from} 00:00:00' AND '{$to} 23:59:00' group by CLICKS.id"
+		from {$prefix}betterlinks_clicks as CLICKS left join {$prefix}betterlinks on {$prefix}betterlinks.id = CLICKS.link_id WHERE created_at BETWEEN '{$from} 00:00:00' AND '{$to} 23:59:00' group by CLICKS.id ORDER BY CLICKS.created_at DESC"
 			)
 			->get();
 
