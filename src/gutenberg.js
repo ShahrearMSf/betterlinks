8c735f802edbf1080a7c4028386d26c1ab1550b8
Tushar Imran <tusharimran9909@gmail.com>
2021-05-30 15:39:44 +0600

    added quick link fields

--- src/gutenberg.js
+++ src/gutenberg.js
@@ -1,13 +1,52 @@
+import { redirectType } from './utils/data';
 const { registerPlugin } = wp.plugins;
 const { __ } = wp.i18n;
-const { Fragment } = wp.element;
+const { Fragment, useEffect } = wp.element;
 const { PluginDocumentSettingPanel, PluginSidebarMoreMenuItem, PluginSidebar } = wp.editPost;
-const { PanelBody, PanelRow, ToggleControl } = wp.components;
+const { PanelBody, PanelRow, ToggleControl, TextControl, SelectControl } = wp.components;
 const { compose } = wp.compose;
 const { withDispatch, withSelect } = wp.data;
 
 const CustomSidebarMetaComponent = (props) => {
-	return <ToggleControl label={__('Enable Instant Redirect', 'betterlinks')} checked={props.customPostMetaValue} onChange={props.setCustomPostMeta} />;
+	useEffect(() => {
+		console.log('init');
+	}, []);
+
+	return (
+		<>
+			<ToggleControl label={__('Enable Instant Redirect', 'betterlinks')} checked={props.customPostMetaValue} onChange={props.setCustomPostMeta} />
+			{props.customPostMetaValue && (
+				<>
+					<TextControl label="Target URL" value="" onChange={(e) => console.log(e)} />
+					<SelectControl
+						label="Redirect Type"
+						options={redirectType}
+						onChange={(e) => {
+							console.log(e);
+						}}
+					/>
+					<SelectControl
+						label="Choose Category"
+						options={[
+							{
+								value: '',
+								label: __('No Category Selected', 'betterlinks'),
+							},
+						]}
+						onChange={(e) => {
+							console.log(e);
+						}}
+					/>
+					<h3>Link Options</h3>
+					<ToggleControl label={__('No Follow', 'betterlinks')} />
+					<ToggleControl label={__('Sponsored', 'betterlinks')} />
+					<ToggleControl label={__('Parameter Forwarding', 'betterlinks')} />
+					<ToggleControl label={__('Tracking', 'betterlinks')} />
+					<h3>Advanced</h3>
+				</>
+			)}
+		</>
+	);
 };
 
 const CustomSidebarMeta = compose([
@@ -33,6 +72,7 @@ const CustomSidebarComponent = () => {
 	);
 };
 
-registerPlugin('awp-customsidebar', {
+registerPlugin('betterlinks-sidebar', {
 	render: CustomSidebarComponent,
+	icon: '',
 });
