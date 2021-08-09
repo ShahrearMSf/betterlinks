7321eb37772584d832b84e6f0b117bee7e559792
Tushar Imran <tusharimran9909@gmail.com>
2021-08-09 10:22:32 +0600

    bug fix - settings data sanitization issue fixing

--- includes/Helper.php
+++ includes/Helper.php
@@ -297,15 +297,15 @@ class Helper
 
     public static function sanitize_text_or_array_field($array_or_string)
     {
-        $boolean = ["true", "false", "1", "0", true, false, 1, 0];
+        $boolean = ["true", "false", "1", "0"];
         if (is_string($array_or_string)) {
-            $array_or_string = in_array($array_or_string, $boolean) ? rest_sanitize_boolean($array_or_string) : sanitize_text_field($array_or_string);
+            $array_or_string = in_array($array_or_string, $boolean) || is_bool($array_or_string) ? rest_sanitize_boolean($array_or_string) : sanitize_text_field($array_or_string);
         } elseif (is_array($array_or_string)) {
             foreach ($array_or_string as $key => &$value) {
                 if (is_array($value)) {
                     $value = self::sanitize_text_or_array_field($value);
                 } else {
-                    $value = in_array($value, $boolean) ? rest_sanitize_boolean($value) : sanitize_text_field($value);
+                    $value = in_array($value, $boolean) || is_bool($value) ? rest_sanitize_boolean($value) : sanitize_text_field($value);
                 }
             }
         }
