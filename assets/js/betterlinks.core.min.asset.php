3847bd77970f0cea04748100980524590c2f5e22
Tushar Imran <tusharimran9909@gmail.com>
2021-08-19 16:23:30 +0600

    bug fix - edit modal close button not working issue fixing

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'a1fdf6a6e8fcda411e0bbf0d7774c9a7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '92c4657f8e8d29b353d896bab48ad809');
