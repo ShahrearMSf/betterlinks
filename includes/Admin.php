c41d7bef64ff4eea693958ec4e5c0e9f0de51121
Tushar Imran <tusharimran9909@gmail.com>
2021-08-22 12:53:39 +0600

    added review notice

--- includes/Admin.php
+++ includes/Admin.php
@@ -3,42 +3,42 @@ namespace BetterLinks;
 
 class Admin
 {
-	public function __construct()
-	{
-		$this->add_menu();
-		$this->add_scripts();
-		add_filter('BetterLinks/Admin/skip_no_conflict', [$this, 'skip_no_conflict']);
-		$this->dispath_notice();
-		$this->usage_tracker();
-		add_filter('plugin_action_links_' . BETTERLINKS_PLUGIN_BASENAME, array($this, 'insert_plugin_links'));
-		add_action('admin_head-toplevel_page_betterlinks', array($this, 'append_no_cache_meta'));
-		add_action('admin_head-toplevel_page_betterlinks-analytics', array($this, 'append_no_cache_meta'));
-		add_action('admin_head-toplevel_page_betterlinks-settings', array($this, 'append_no_cache_meta'));
-	}
-	public function add_menu()
-	{
-		new Admin\Menu();
-	}
-	public function add_scripts()
-	{
-		new Admin\Assets();
-	}
-	public function dispath_notice()
-	{
-		new Admin\Ajax();
-		new Admin\Notice();
-	}
-	public function skip_no_conflict()
-	{
-		$whitelist = ['127.0.0.1', '::1'];
-		if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
-			return true;
-		}
-		return false;
-	}
-	public function usage_tracker()
+    public function __construct()
     {
-		$tracker = Admin\WPDev\PluginUsageTracker::get_instance(BETTERLINKS_PLUGIN_FILE, [
+        $this->add_menu();
+        $this->add_scripts();
+        $this->dispath_action();
+        $this->usage_tracker();
+    }
+    public function add_menu()
+    {
+        new Admin\Menu();
+    }
+    public function add_scripts()
+    {
+        new Admin\Assets();
+    }
+    public function dispath_action()
+    {
+        new Admin\Ajax();
+        new Admin\Notice();
+        add_filter('BetterLinks/Admin/skip_no_conflict', [$this, 'skip_no_conflict']);
+        add_filter('plugin_action_links_' . BETTERLINKS_PLUGIN_BASENAME, array($this, 'insert_plugin_links'));
+        add_action('admin_head-toplevel_page_betterlinks', array($this, 'append_no_cache_meta'));
+        add_action('admin_head-toplevel_page_betterlinks-analytics', array($this, 'append_no_cache_meta'));
+        add_action('admin_head-toplevel_page_betterlinks-settings', array($this, 'append_no_cache_meta'));
+    }
+    public function skip_no_conflict()
+    {
+        $whitelist = ['127.0.0.1', '::1'];
+        if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+            return true;
+        }
+        return false;
+    }
+    public function usage_tracker()
+    {
+        $tracker = Admin\WPDev\PluginUsageTracker::get_instance(BETTERLINKS_PLUGIN_FILE, [
             'opt_in'       => true,
             'goodbye_form' => true,
             'item_id'      => '720bbe6537bffcb73f37',
@@ -49,17 +49,17 @@ class Admin
         ));
         $tracker->init();
     }
-	public function insert_plugin_links($links)
-	{
+    public function insert_plugin_links($links)
+    {
         if (!apply_filters('betterlinks/pro_enabled', false)) {
             $links[] = sprintf('<a href="https://wpdeveloper.net/in/upgrade-betterlinks" target="_blank" style="color: #39b54a; font-weight: bold;">' . __('Go Pro', 'betterlinks') . '</a>');
         }
         return $links;
-	}
-	public function append_no_cache_meta()
-	{
-		echo '<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    }
+    public function append_no_cache_meta()
+    {
+        echo '<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 		<meta http-equiv="Pragma" content="no-cache">
 		<meta http-equiv="Expires" content="0">';
-	}
+    }
 }
--- includes/Admin/Notice.php
+++ includes/Admin/Notice.php
@@ -4,69 +4,148 @@ namespace BetterLinks\Admin;
 
 class Notice
 {
-	public $pagenow;
-	public function __construct()
-	{
-		$this->prettylinks_notice();
-		$this->simple_301_redirects_notice();
-	}
-	public function prettylinks_notice()
-	{
-		if (defined('PRLI_VERSION') && !get_option('betterlinks_notice_ptl_migrate')) {
-			global $pagenow;
-			$this->pagenow = $pagenow;
-			if (!get_option('betterlinks_hide_notice_ptl_migrate')) {
-				add_action('admin_notices', [$this, 'prettylinks_migration_notice']);
-				add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
-			} elseif ($pagenow === 'admin.php') {
-				add_action('admin_notices', [$this, 'prettylinks_migration_notice']);
-				add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
-			}
-		} elseif (defined('PRLI_VERSION') && get_option('betterlinks_notice_ptl_migrate')) {
-			global $pagenow;
-			$this->pagenow = $pagenow;
-			if (!get_option('betterlinks_hide_notice_ptl_deactive')) {
-				if(!isset($_GET['post_type']) || (isset($_GET['post_type']) && $_GET['post_type'] !== 'pretty-link')){ 
-					add_action('admin_notices', [$this, 'prettylinks_deactive_notice']);
-				}
-				add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
-			}
-		}
-	}
+    public $pagenow;
+    public function __construct()
+    {
+        $this->review_notice();
+        $this->prettylinks_notice();
+        $this->simple_301_redirects_notice();
+    }
+    public function review_notice()
+    {
+        $notice = new WPDev\WPDevNotice(BETTERLINKS_PLUGIN_BASENAME, BETTERLINKS_VERSION);
 
-	public function simple_301_redirects_notice()
-	{
-		global $pagenow;
-		$this->pagenow = $pagenow;
-		if (defined('SIMPLE301REDIRECTS_VERSION') && !get_option('betterlinks_notice_s301r_migrate')) {
-			if (!get_option('betterlinks_hide_notice_s301r_migrate')) {
-				add_action('admin_notices', [$this, 'simple301redirects_migration_notice']);
-				add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
-			}
-		} elseif (defined('SIMPLE301REDIRECTS_VERSION') && get_option('betterlinks_notice_s301r_migrate')) {
-			if (!get_option('betterlinks_hide_notice_s301r_deactive')) {
-				if(!isset($_GET['page']) || (isset($_GET['page']) && $_GET['page'] !== '301options')){
-					add_action('admin_notices', [$this, 'simple301redirects_deactive_notice']);
-				}
-				add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
-			}
-		}
-	}
+        /**
+         * Current Notice End Time.
+         * Notice will dismiss in 3 days if user does nothing.
+         */
+        $notice->cne_time = '3 Day';
+        /**
+         * Current Notice Maybe Later Time.
+         * Notice will show again in 7 days
+         */
+        $notice->maybe_later_time = '7 Day';
 
-	public function prettylinks_migration_notice()
-	{
-		?>
+        $notice->text_domain = 'betterlinks';
+
+        $scheme = (parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY)) ? '&' : '?';
+        $url = $_SERVER['REQUEST_URI'] . $scheme;
+        $notice->links = [
+            'review' => array(
+                'later' => array(
+                    'link' => 'https://wordpress.org/plugins/betterlinks/#reviews',
+                    'target' => '_blank',
+                    'label' => __('Ok, you deserve it!', 'betterlinks'),
+                    'icon_class' => 'dashicons dashicons-external',
+                ),
+                'allready' => array(
+                    'link' => $url,
+                    'label' => __('I already did', 'betterlinks'),
+                    'icon_class' => 'dashicons dashicons-smiley',
+                    'data_args' => [
+                        'dismiss' => true,
+                    ],
+                ),
+                'maybe_later' => array(
+                    'link' => $url,
+                    'label' => __('Maybe Later', 'betterlinks'),
+                    'icon_class' => 'dashicons dashicons-calendar-alt',
+                    'data_args' => [
+                        'later' => true,
+                    ],
+                ),
+                'support' => array(
+                    'link' => 'https://wpdeveloper.net/support',
+                    'label' => __('I need help', 'betterlinks'),
+                    'icon_class' => 'dashicons dashicons-sos',
+                ),
+                'never_show_again' => array(
+                    'link' => $url,
+                    'label' => __('Never show again', 'betterlinks'),
+                    'icon_class' => 'dashicons dashicons-dismiss',
+                    'data_args' => [
+                        'dismiss' => true,
+                    ],
+                ),
+            ),
+        ];
+
+        /**
+         * This is review message and thumbnail.
+         */
+        global $betterlinks;
+        $current_user = wp_get_current_user();
+        $total_links = (is_array($betterlinks) && isset($betterlinks['links']) ? count($betterlinks['links']) : 0);
+        $notice->message('review', '<p>'.esc_html__('Howdy, ', 'betterlinks') . $current_user->user_login . esc_html__('! 👋 You have created ', 'betterlinks'). $total_links.' '.esc_html__('Shortened URLs so far 🎉 If you are enjoying using BetterLinks, feel free to leave a 5* Review on the WordPress Forum.', 'betterlinks').'</p>');
+        $notice->thumbnail('review', plugins_url('assets/images/logo-large.svg', BETTERLINKS_PLUGIN_BASENAME));
+        
+        $notice->options_args = array(
+            'notice_will_show' => [
+                'opt_in' => $notice->timestamp,
+                'upsale' => $notice->makeTime($notice->timestamp, '7 Day'),
+                'review' => $notice->makeTime($notice->timestamp, '3 Day'), // after 3 days
+            ],
+        );
+        // main notice init
+        $notice->init();
+    }
+    public function prettylinks_notice()
+    {
+        if (defined('PRLI_VERSION') && !get_option('betterlinks_notice_ptl_migrate')) {
+            global $pagenow;
+            $this->pagenow = $pagenow;
+            if (!get_option('betterlinks_hide_notice_ptl_migrate')) {
+                add_action('admin_notices', [$this, 'prettylinks_migration_notice']);
+                add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
+            } elseif ($pagenow === 'admin.php') {
+                add_action('admin_notices', [$this, 'prettylinks_migration_notice']);
+                add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
+            }
+        } elseif (defined('PRLI_VERSION') && get_option('betterlinks_notice_ptl_migrate')) {
+            global $pagenow;
+            $this->pagenow = $pagenow;
+            if (!get_option('betterlinks_hide_notice_ptl_deactive')) {
+                if (!isset($_GET['post_type']) || (isset($_GET['post_type']) && $_GET['post_type'] !== 'pretty-link')) {
+                    add_action('admin_notices', [$this, 'prettylinks_deactive_notice']);
+                }
+                add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
+            }
+        }
+    }
+
+    public function simple_301_redirects_notice()
+    {
+        global $pagenow;
+        $this->pagenow = $pagenow;
+        if (defined('SIMPLE301REDIRECTS_VERSION') && !get_option('betterlinks_notice_s301r_migrate')) {
+            if (!get_option('betterlinks_hide_notice_s301r_migrate')) {
+                add_action('admin_notices', [$this, 'simple301redirects_migration_notice']);
+                add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
+            }
+        } elseif (defined('SIMPLE301REDIRECTS_VERSION') && get_option('betterlinks_notice_s301r_migrate')) {
+            if (!get_option('betterlinks_hide_notice_s301r_deactive')) {
+                if (!isset($_GET['page']) || (isset($_GET['page']) && $_GET['page'] !== '301options')) {
+                    add_action('admin_notices', [$this, 'simple301redirects_deactive_notice']);
+                }
+                add_action('admin_print_footer_scripts', [$this, 'admin_notice_scripts']);
+            }
+        }
+    }
+
+    public function prettylinks_migration_notice()
+    {
+        ?>
         <div class="notice notice-info betterlinks-notice-pt-migrate <?php echo $this->pagenow !== 'admin.php' ? 'is-dismissible' : ''; ?>">
             <p>
                 <?php _e('Whoops! You are already using Pretty Links on your website. To migrate your Pretty Links data to BetterLinks, click here.', 'betterlinks'); ?>
-                <a href="<?php echo esc_url(admin_url('admin.php?page=betterlinks-settings&migration=prettylinks')); ?>" class="button button-primary"><?php _e('Start Migration','betterlinks'); ?></a>
+                <a href="<?php echo esc_url(admin_url('admin.php?page=betterlinks-settings&migration=prettylinks')); ?>" class="button button-primary"><?php _e('Start Migration', 'betterlinks'); ?></a>
             </p>
         </div>
         <?php
-	}
-	public function prettylinks_deactive_notice()
-	{
-		?>
+    }
+    public function prettylinks_deactive_notice()
+    {
+        ?>
         <div class="notice notice-error betterlinks-notice-deactive-prettylinks <?php echo $this->pagenow !== 'admin.php' ? 'is-dismissible' : ''; ?>">
             <p>
                 <?php _e('All Pretty Links have been successfully migrated to BetterLinks. You can now safely deactivate Pretty Links on your website.', 'betterlinks'); ?>
@@ -74,25 +153,25 @@ class Notice
             </p>
         </div>
         <?php
-	}
+    }
 
-	public function simple301redirects_migration_notice()
-	{
-		?>
+    public function simple301redirects_migration_notice()
+    {
+        ?>
         <div class="notice notice-info betterlinks-notice-simple301redirects-migrate <?php echo $this->pagenow !== 'admin.php' ? 'is-dismissible' : ''; ?>">
             <p>
                 <?php _e('Whoops! You are already using Simple 301 Redirects on your website. To migrate your Simple 301 Redirects data to BetterLinks, click here.', 'betterlinks'); ?>
                 <a href="<?php echo esc_url(admin_url('admin.php?page=betterlinks-settings&migration=simple301redirects')); ?>" class="button button-primary"><?php _e(
-				'Start Migration',
-				'betterlinks'
-			); ?></a>
+            'Start Migration',
+            'betterlinks'
+        ); ?></a>
             </p>
         </div>
         <?php
-	}
-	public function simple301redirects_deactive_notice()
-	{
-		?>
+    }
+    public function simple301redirects_deactive_notice()
+    {
+        ?>
         <div class="notice notice-error betterlinks-notice-deactive-simple301redirects <?php echo $this->pagenow !== 'admin.php' ? 'is-dismissible' : ''; ?>">
             <p>
                 <?php _e('All Simple 301 Redirects have been successfully migrated to BetterLinks. You can now safely deactivate Simple 301 Redirects on your website.', 'betterlinks'); ?>
@@ -100,11 +179,11 @@ class Notice
             </p>
         </div>
         <?php
-	}
+    }
 
-	public function admin_notice_scripts()
-	{
-		$nonce = wp_create_nonce('betterlinks_admin_nonce'); ?>
+    public function admin_notice_scripts()
+    {
+        $nonce = wp_create_nonce('betterlinks_admin_nonce'); ?>
 		<script type='text/javascript'>
 		jQuery( document ).ready(function() {
 			jQuery('.betterlinks-notice-deactive-prettylinks a.deactive').on('click', function(e){
@@ -162,5 +241,5 @@ class Notice
 		});
 		</script>
 		<?php
-	}
+    }
 }
