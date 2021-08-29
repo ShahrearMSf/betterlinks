e0bcf432fb2005a8e8d99b67d10a7be1a177baca
Tushar Imran <tusharimran9909@gmail.com>
2021-08-29 11:02:04 +0600

    Bug Fixed - fresh installation db migration issue fixing

--- includes/Installer.php
+++ includes/Installer.php
@@ -2,197 +2,199 @@
 namespace BetterLinks;
 
 class Installer extends \WP_Background_Process
-{ 
+{
     use Traits\DBTables;
     use Traits\DBMigrate;
     protected $wpdb;
-	protected $charset_collate;
+    protected $charset_collate;
     protected $action;
     public $activation;
     public $migration;
     public $db_version;
 
     public function __construct()
-	{
-		parent::__construct();
-		global $wpdb;
-		$this->wpdb = $wpdb;
-		$this->charset_collate = $wpdb->get_charset_collate();
-		$this->action = 'betterlinks_background_task';
-		$this->activation = ['create_db_tables', 'db_migration', 'insert_terms_data','create_json_files','save_settings','update_json_links'];
+    {
+        parent::__construct();
+        global $wpdb;
+        $this->wpdb = $wpdb;
+        $this->charset_collate = $wpdb->get_charset_collate();
+        $this->action = 'betterlinks_background_task';
+        $this->activation = ['create_db_tables', 'db_migration', 'insert_terms_data','create_json_files','save_settings','update_json_links'];
         $this->migration = ['db_migration', 'update_json_links', 'clear_cache'];
         $this->db_version = get_option('betterlinks_db_version');
-	}
+    }
 
     public function start_dispatch()
-	{
-		if(get_option($this->action)){
-			delete_option($this->action);
-			return true;
-		}
-		return false;
-	}
-	public function doing_dispatch()
-	{
-		return get_option($this->action);
+    {
+        if (get_option($this->action)) {
+            delete_option($this->action);
+            return true;
+        }
+        return false;
     }
-	public function init()
-	{
-		add_option($this->action, true);
+    public function doing_dispatch()
+    {
+        return get_option($this->action);
+    }
+    public function init()
+    {
+        add_option($this->action, true);
     }
     
     /**
-	 * Task
-	 *
-	 * Override this method to perform any actions required on each
-	 * queue item. Return the modified item for further processing
-	 * in the next pass through. Or, return false to remove the
-	 * item from the queue.
-	 *
-	 * @param mixed $item Queue item to iterate over
-	 *
-	 * @return mixed
-	 */
-	protected function task( $item ) {
-		if(method_exists($this, $item)){
-			try {
-				$this->$item();
-			} catch ( \Exception $e ) {
-				if ( defined('WP_DEBUG') && WP_DEBUG) {
-					trigger_error( 'BetterLinks background task triggered fatal error for callback ' . esc_html( $item ), E_USER_WARNING ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
-				}
+     * Task
+     *
+     * Override this method to perform any actions required on each
+     * queue item. Return the modified item for further processing
+     * in the next pass through. Or, return false to remove the
+     * item from the queue.
+     *
+     * @param mixed $item Queue item to iterate over
+     *
+     * @return mixed
+     */
+    protected function task($item)
+    {
+        if (method_exists($this, $item)) {
+            try {
+                $this->$item();
+            } catch (\Exception $e) {
+                if (defined('WP_DEBUG') && WP_DEBUG) {
+                    trigger_error('BetterLinks background task triggered fatal error for callback ' . esc_html($item), E_USER_WARNING); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
+                }
             }
         }
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Complete
-	 *
-	 * Override if applicable, but ensure that the below actions are
-	 * performed, or, call parent::complete().
-	 */
-	protected function complete() {
-		parent::complete();
-		// Show notice to user or perform some other arbitrary task...
+    /**
+     * Complete
+     *
+     * Override if applicable, but ensure that the below actions are
+     * performed, or, call parent::complete().
+     */
+    protected function complete()
+    {
+        parent::complete();
+        // Show notice to user or perform some other arbitrary task...
     }
     
     public function create_db_tables()
-	{
-		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-		$this->createBetterLinksTable();
-		$this->createBetterTermsTable();
-		$this->createBetterTermsRelationshipsTable();
-		$this->createBetterClicksTable();
-		// update plugin version
-		if (!get_option('betterlinks_version')) {
-			update_option('betterlinks_version', BETTERLINKS_VERSION);
-		}
-		// update db version
+    {
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        $this->createBetterLinksTable();
+        $this->createBetterTermsTable();
+        $this->createBetterTermsRelationshipsTable();
+        $this->createBetterClicksTable();
+        // update plugin version
+        if (!get_option('betterlinks_version')) {
+            update_option('betterlinks_version', BETTERLINKS_VERSION);
+        }
+        // update db version
         if (!get_option('betterlinks_db_version')) {
-			update_option('betterlinks_db_version', BETTERLINKS_DB_VERSION);
-		}
-	}
+            update_option('betterlinks_db_version', BETTERLINKS_DB_VERSION);
+        }
+    }
 
-	public function insert_terms_data()
-	{
-		$query = \BetterLinks\Helper::DB();
-		$result = $query
-			->table('betterlinks_terms')
-			->where('term_slug', '=', 'uncategorized')
-			->get();
-		if (count($result) === 0) {
-			try {
-				$data = [
-					'term_name' => 'Uncategorized',
-					'term_slug' => 'uncategorized',
-					'term_type' => 'category',
-				];
-				$query
-					->table('betterlinks_terms')
-					->where('term_slug', '!', 'uncategorized')
-					->insert($data);
-			} catch (\Throwable $th) {
-				echo $th->getMessage();
-			}
-		}
-	}
+    public function insert_terms_data()
+    {
+        $query = \BetterLinks\Helper::DB();
+        $result = $query
+            ->table('betterlinks_terms')
+            ->where('term_slug', '=', 'uncategorized')
+            ->get();
+        if (count($result) === 0) {
+            try {
+                $data = [
+                    'term_name' => 'Uncategorized',
+                    'term_slug' => 'uncategorized',
+                    'term_type' => 'category',
+                ];
+                $query
+                    ->table('betterlinks_terms')
+                    ->where('term_slug', '!', 'uncategorized')
+                    ->insert($data);
+            } catch (\Throwable $th) {
+                echo $th->getMessage();
+            }
+        }
+    }
 
-	public function save_settings()
-	{
-		if (!get_option(BETTERLINKS_LINKS_OPTION_NAME)) {
-			$value = [
-				'redirect_type' 	=> '307',
-				'nofollow' 			=> true,
-				'sponsored' 		=> '',
-				'track_me' 			=> true,
-				'param_forwarding' 	=> '',
-				'wildcards' 		=> false,
-				'disablebotclicks' 	=> false, 
-				'is_allow_gutenberg'=> true,
-				'force_https'		=> false
-			];
-			add_option(BETTERLINKS_LINKS_OPTION_NAME, json_encode($value));
-		}
-	}
+    public function save_settings()
+    {
+        if (!get_option(BETTERLINKS_LINKS_OPTION_NAME)) {
+            $value = [
+                'redirect_type' 	=> '307',
+                'nofollow' 			=> true,
+                'sponsored' 		=> '',
+                'track_me' 			=> true,
+                'param_forwarding' 	=> '',
+                'wildcards' 		=> false,
+                'disablebotclicks' 	=> false,
+                'is_allow_gutenberg'=> true,
+                'force_https'		=> false
+            ];
+            add_option(BETTERLINKS_LINKS_OPTION_NAME, json_encode($value));
+        }
+    }
 
-	/**
-	 * Create files/directories.
-	 */
-	public function create_json_files()
-	{
-		$emptyContent = '{}';
-		$files = [
-			[
-				'base' => BETTERLINKS_UPLOAD_DIR_PATH,
-				'file' => 'index.html',
-				'content' => '',
-			],
-			[
-				'base' => BETTERLINKS_UPLOAD_DIR_PATH,
-				'file' => 'links.json',
-				'content' => $emptyContent,
-			],
-			[
-				'base' => BETTERLINKS_UPLOAD_DIR_PATH,
-				'file' => 'clicks.json',
-				'content' => $emptyContent,
-			],
-		];
+    /**
+     * Create files/directories.
+     */
+    public function create_json_files()
+    {
+        $emptyContent = '{}';
+        $files = [
+            [
+                'base' => BETTERLINKS_UPLOAD_DIR_PATH,
+                'file' => 'index.html',
+                'content' => '',
+            ],
+            [
+                'base' => BETTERLINKS_UPLOAD_DIR_PATH,
+                'file' => 'links.json',
+                'content' => $emptyContent,
+            ],
+            [
+                'base' => BETTERLINKS_UPLOAD_DIR_PATH,
+                'file' => 'clicks.json',
+                'content' => $emptyContent,
+            ],
+        ];
 
-		foreach ($files as $file) {
-			if (wp_mkdir_p($file['base']) && !file_exists(trailingslashit($file['base']) . $file['file'])) {
-				$file_handle = @fopen(trailingslashit($file['base']) . $file['file'], 'wb');
-				if ($file_handle) {
-					fwrite($file_handle, $file['content']);
-					fclose($file_handle);
-				}
-			}
-		}
-	}
+        foreach ($files as $file) {
+            if (wp_mkdir_p($file['base']) && !file_exists(trailingslashit($file['base']) . $file['file'])) {
+                $file_handle = @fopen(trailingslashit($file['base']) . $file['file'], 'wb');
+                if ($file_handle) {
+                    fwrite($file_handle, $file['content']);
+                    fclose($file_handle);
+                }
+            }
+        }
+    }
 
-	public function update_json_links()
-	{
-		$Cron = new Cron();
-		$Cron->write_json_links();
+    public function update_json_links()
+    {
+        $Cron = new Cron();
+        $Cron->write_json_links();
     }
     
     public function db_migration()
     {
-        if ($this->db_version != BETTERLINKS_DB_VERSION) {
-            if(BETTERLINKS_DB_VERSION == '1.1'){
+        if ($this->db_version && $this->db_version != BETTERLINKS_DB_VERSION) {
+            if (BETTERLINKS_DB_VERSION == '1.1') {
                 $this->db_migration_1_1();
-            }else if(BETTERLINKS_DB_VERSION == '1.2'){
+            } elseif (BETTERLINKS_DB_VERSION == '1.2') {
                 $this->db_migration_1_2();
-            }else if(BETTERLINKS_DB_VERSION == '1.4'){
+            } elseif (BETTERLINKS_DB_VERSION == '1.4') {
                 $this->db_migration_1_4();
-			}
-            if(version_compare($this->db_version, '1.3', '<')){
+            }
+            if (version_compare($this->db_version, '1.3', '<')) {
                 $this->db_migration_1_1();
                 $this->db_migration_1_2();
             }
-			update_option('betterlinks_db_version', BETTERLINKS_DB_VERSION);
-		}
+        }
+        update_option('betterlinks_db_version', BETTERLINKS_DB_VERSION);
     }
 
     public function clear_cache()
