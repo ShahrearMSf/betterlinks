0c3ffef638c4538d99434a01d7ada91bbf02708b
Tushar Imran <tusharimran9909@gmail.com>
2021-09-01 13:52:16 +0600

    added link csv export download system

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '486611b248426defa3d19946a0ed0609');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '4664557b0d53541dc84548e4bf5fbdfa');
