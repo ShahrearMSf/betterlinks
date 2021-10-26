3a39e3b7a23fbd0c2be822a00c49809d9fc358e1
Tushar Imran <tusharimran9909@gmail.com>
2021-10-26 12:17:30 +0600

    bug fixed - admin sub menu item assets loading issue fixing

--- includes/Helper.php
+++ includes/Helper.php
@@ -77,7 +77,7 @@ class Helper
             return true;
         } else {
             foreach (self::get_menu_items() as $key => $value) {
-                if (strpos($hook, BETTERLINKS_PLUGIN_SLUG . '_page_' . $key)) {
+                if ($hook === BETTERLINKS_PLUGIN_SLUG . '_page_' . $key || strpos($hook, BETTERLINKS_PLUGIN_SLUG . '_page_' . $key)) {
                     return true;
                 }
             }
