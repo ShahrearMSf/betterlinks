aa50c1111605c50328e12753ab8845b5c9b48486
Linkon Miyan <rudlinkon@gmail.com>
2022-01-04 15:40:52 +0600

    Added initial controller in page settings tab

--- includes/Elementor.php
+++ includes/Elementor.php
@@ -1,8 +1,27 @@
 <?php
 namespace BetterLinks;
 
+use Elementor\Controls_Manager;
+
 class Elementor {
 	public function __construct() {
+		add_action( 'elementor/element/before_section_end', [ $this, 'add_controller' ], 10, 3 );
+	}
 
+	public function add_controller( $document, $section_id, $args ) {
+		if ( $section_id === 'document_settings' && $args['tab'] === 'settings' ) {
+			$this->instant_redirect_controls( $document );
+		}
+	}
+
+	public function instant_redirect_controls( $controls ) {
+		$controls->add_control(
+			'bl_instant_redirect_heading',
+			[
+				'type'      => Controls_Manager::HEADING,
+				'label'     => __( 'BetterLinks Instant Redirect', 'betterlinks' ),
+				'separator' => 'before',
+			]
+		);
 	}
 }
