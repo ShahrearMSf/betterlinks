6d2f293c3338812b54237a301136c1a8dfe60c4b
Linkon Miyan <rudlinkon@gmail.com>
2022-01-04 15:42:15 +0600

    added link options helper method to get the options data

--- includes/Elementor.php
+++ includes/Elementor.php
@@ -8,6 +8,20 @@ class Elementor {
 		add_action( 'elementor/element/before_section_end', [ $this, 'add_controller' ], 10, 3 );
 	}
 
+	public function bl_get_link_options( $option_name = null ) {
+		$links_option = json_decode( get_option( BETTERLINKS_LINKS_OPTION_NAME ), true );
+
+		if ( $option_name ) {
+			if ( isset( $links_option[ $option_name ] ) ) {
+				return $links_option[ $option_name ];
+			}
+
+			return '';
+		}
+
+		return $links_option;
+	}
+
 	public function add_controller( $document, $section_id, $args ) {
 		if ( $section_id === 'document_settings' && $args['tab'] === 'settings' ) {
 			$this->instant_redirect_controls( $document );
