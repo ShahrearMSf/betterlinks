74e3402af79a7bbc91bf637eb856a6ed70233990
hz-tyfoon <md.tifun@gmail.com>
2022-07-07 15:56:11 +0600

    added localized 'is_pro_enabled' in gutenberg editor

--- includes/Admin/Assets.php
+++ includes/Admin/Assets.php
@@ -97,6 +97,7 @@ class Assets
             'plugin_root_path' => BETTERLINKS_ROOT_DIR_PATH,
             'site_url' => site_url(),
             'route_path' => parse_url(admin_url(), PHP_URL_PATH),
+            'is_pro_enabled' => apply_filters('betterlinks/pro_enabled', false),
         ]);
         wp_set_script_translations('betterlinks-gutenberg', 'betterlinks', BETTERLINKS_ROOT_DIR_PATH . 'languages/');
     }
