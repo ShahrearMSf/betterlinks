9e18f695fc453fda1fce2be3496dc31d08b231ef
hz-tyfoon <md.tifun@gmail.com>
2022-08-23 15:52:27 +0600

    gutenberg format: added tooltip for betterlinks preview bar button icons

--- assets/js/betterlinks-gutenberg.core.min.asset.php
+++ assets/js/betterlinks-gutenberg.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '96c79c6368bd258394c1e0237dffb08f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '71052f307432e49dd09e9995aa08a398');
--- dev_betterlinks/gutenberg/formats/betterlinks/linkPreview.js
+++ dev_betterlinks/gutenberg/formats/betterlinks/linkPreview.js
@@ -1,11 +1,15 @@
 const {
-	// useState,
+	useState,
 	useEffect,
 	useRef,
 	// useMemo,
 } = wp.element;
 
+const { Popover, Button } = wp.components;
+
 export const LinkPreview = ({ close, activeAttributes }) => {
+	const [showFormatTooltip, setShowFormatTooltip] = useState(false);
+
 	const btnRef = useRef(null);
 
 	useEffect(() => {
@@ -18,14 +22,21 @@ export const LinkPreview = ({ close, activeAttributes }) => {
 			getSelection().removeAllRanges();
 		}
 
-		//
-		if (btnRef?.current) {
-			btnRef.current.focus();
+		const btnEl = btnRef?.current;
+		if (btnEl) {
+			btnEl.focus();
+			btnEl.closest('body').classList.add('betterlinks-format-link-preview-mounted');
 		}
+		console.log('----LinkPreview useEffec runned with []', { btnEl });
 
 		return () => {
-			console.log('----LinkPreview component cleanup runned');
 			close();
+			const btnEl = btnRef?.current;
+			if (btnEl) {
+				btnEl.focus();
+				btnEl.closest('body').classList.remove('betterlinks-format-link-preview-mounted');
+			}
+			console.log('----LinkPreview component cleanup runned', { btnEl });
 		};
 	}, []);
 
@@ -46,12 +57,20 @@ export const LinkPreview = ({ close, activeAttributes }) => {
 				<i class="btl btl-visit-url"></i>
 			</a>
 
-			<button className="betterlinks-format-change-link" onClick={handleChangeLink} ref={btnRef}>
+			<button className="betterlinks-format-change-link btl-tooltip" onClick={handleChangeLink} ref={btnRef}>
 				<span className="dashicons dashicons-edit"></span>
+				<span class="btl-tooltiptext">Change Link</span>
 			</button>
 
-			<button className="betterlinks-format-edit-link icon" onClick={handleEditBetterLink} ref={btnRef}>
+			<button
+				className="betterlinks-format-edit-link icon btl-tooltip"
+				onMouseEnter={() => setShowFormatTooltip(true)}
+				onMouseLeave={() => setShowFormatTooltip(false)}
+				onClick={handleEditBetterLink}
+				ref={btnRef}
+			>
 				<i class="btl btl-edit"></i>
+				<span class="btl-tooltiptext">Edit In Betterlinks</span>
 			</button>
 		</div>
 	);
