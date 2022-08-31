1259c8af1f77414da76c18ab20affb59bb63083a
hz-tyfoon <md.tifun@gmail.com>
2022-08-31 11:07:20 +0600

    gutenberg format: replaced '<keyboardReturn />' component with custom svg

--- assets/js/betterlinks-gutenberg.core.min.asset.php
+++ assets/js/betterlinks-gutenberg.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '428a8087fbf60558dc77da462a93efdf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '64ead0f1847276635927d897d2d48de9');
--- dev_betterlinks/gutenberg/formats/betterlinks/index.js
+++ dev_betterlinks/gutenberg/formats/betterlinks/index.js
@@ -2,11 +2,10 @@
 const { __ } = wp.i18n;
 const { applyFormat, removeFormat, create, insert, isCollapsed } = wp.richText;
 const { useState, useEffect, useRef, useMemo } = wp.element;
-const { Popover, Button, ToggleControl } = wp.components;
+const { Popover, ToggleControl } = wp.components;
 const { RichTextToolbarButton, URLPopover, RichTextShortcut } = wp.blockEditor;
 const { UP, DOWN, ENTER } = wp.keycodes;
 const { getRectangleFromRange } = wp.dom;
-import { keyboardReturn } from '@wordpress/icons';
 
 // external library imports
 import reactStringReplace from 'react-string-replace';
@@ -358,7 +357,11 @@ export const betterlinksFormat = {
 													</Popover>
 												)}
 
-												<Button className="btl-submit-button" icon={keyboardReturn} label={__('Apply')} type="submit" />
+												<button type="submit" class="components-button btl-submit-button">
+													<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" aria-hidden="true" focusable="false">
+														<path d="M6.734 16.106l2.176-2.38-1.093-1.028-3.846 4.158 3.846 4.157 1.093-1.027-2.176-2.38h2.811c1.125 0 2.25.03 3.374 0 1.428-.001 3.362-.25 4.963-1.277 1.66-1.065 2.868-2.906 2.868-5.859 0-2.479-1.327-4.896-3.65-5.93-1.82-.813-3.044-.8-4.806-.788l-.567.002v1.5c.184 0 .368 0 .553-.002 1.82-.007 2.704-.014 4.21.657 1.854.827 2.76 2.657 2.76 4.561 0 2.472-.973 3.824-2.178 4.596-1.258.807-2.864 1.04-4.163 1.04h-.02c-1.115.03-2.229 0-3.344 0H6.734z"></path>
+													</svg>
+												</button>
 											</form>
 										</>
 									)}
