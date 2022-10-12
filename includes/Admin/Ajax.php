0dda5db4d0449b1c5bca6e4a63aa8d39dcf4f2d2
hz-tyfoon <md.tifun@gmail.com>
2022-10-12 18:37:18 +0600

    migration (pretty-link): renamed 'variable names' & 'array keys' in 'get_prettylinks_data method' as they're now recieving only the 'number' of 'links' & 'clicks' rather than 'everything from those 2 tables'

--- includes/Admin/Ajax.php
+++ includes/Admin/Ajax.php
@@ -63,10 +63,10 @@ class Ajax
         if (!current_user_can('manage_options')) {
             wp_die();
         }
-        $links = \BetterLinks\Helper::get_prettylinks_links();
-        $clicks = \BetterLinks\Helper::get_prettylinks_clicks();
-        set_transient('betterlinks_migration_data_prettylinks', ['links' => $links, 'clicks' => $clicks], 60 * 5);
-        wp_send_json_success(['links' => $links, 'clicks' => $clicks]);
+        $links_count = \BetterLinks\Helper::get_prettylinks_links();
+        $clicks_count = \BetterLinks\Helper::get_prettylinks_clicks();
+        set_transient('betterlinks_migration_data_prettylinks', ['links_count' => $links_count, 'clicks_count' => $clicks_count], 60 * 5);
+        wp_send_json_success(['links_count' => $links_count, 'clicks_count' => $clicks_count]);
         wp_die();
     }
 
