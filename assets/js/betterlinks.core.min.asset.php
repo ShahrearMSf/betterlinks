ef2984dd1f4b5e5f84b8ce09a21cfe923ae7e345
hz-tyfoon <md.tifun@gmail.com>
2022-11-06 16:05:30 +0600

    migration (pretty-link): icon for 'migration running in background' changed

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '4c3ce3c0793d47bff9f68792d1e14fc1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'e13a54b1d6e4eb67fbf7');
