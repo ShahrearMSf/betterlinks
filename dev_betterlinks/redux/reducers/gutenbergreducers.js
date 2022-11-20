aa7a0cc1e1cd068ae37acee918ae2f3c185ca086
hz-tyfoon <md.tifun@gmail.com>
2022-11-20 14:22:04 +0600

    fixed: module import failing for case sensitivity

--- dev_betterlinks/redux/reducers/gutenbergreducers.js
+++ dev_betterlinks/redux/reducers/gutenbergreducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import settings from 'redux/reducers/settings.reducers';
 import links from 'redux/reducers/links.reducers';
-import gutenbergredirectlink from 'redux/reducers/gutenbergRedirectLink.reducers';
+import gutenbergredirectlink from 'redux/reducers/gutenbergredirectlink.reducers';
 import terms from 'redux/reducers/terms.reducers';
 
 export const gutenbergReducers = combineReducers({
