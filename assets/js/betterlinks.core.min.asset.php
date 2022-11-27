56313d46d4b990c89e348ef43130388ca70a836b
hz-tyfoon <md.tifun@gmail.com>
2022-11-27 21:24:26 +0600

    betterlinks settings: connected posttypes datas of store to settings component to use it for filter

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'a48f76667f54cea53c73');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'cd6eac372ca5c6444df8');
