449031314e16c2711316d3cffefd4a5dcb403845
hz-tyfoon <md.tifun@gmail.com>
2022-12-22 11:56:12 +0600

    fixed: analytics not showing at all

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'd000a576ef334a3445d4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '30233cf2ac99b3c838f0');
