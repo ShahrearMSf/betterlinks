ab0db305d5c4bebd625319c216c958f018c20e37
hz-tyfoon <md.tifun@gmail.com>
2022-12-29 10:13:01 +0600

    fixed: 'join the community' link to wpdevelopers' facebook group

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '92c5aa3bb6f2181e926c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'd4e28682d442971304c8');
