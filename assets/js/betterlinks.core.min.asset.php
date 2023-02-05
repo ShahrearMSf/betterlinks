fca5cbd7d06cf3c645cc27ac569035f34b421567
hz-tyfoon <md.tifun@gmail.com>
2023-02-05 20:39:05 +0600

    Betterlinks Analytics: designed/styled initial popup select/apply button

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '97020165f4b79982f7c0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '11711dfe0d4169a15447');
--- dev_betterlinks/containers/DeleteClicks.js
+++ dev_betterlinks/containers/DeleteClicks.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { __ } from '@wordpress/i18n';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -6,6 +6,22 @@ import { deleteClicks, formatDate } from 'utils/helper';
 import { fetchCustomClicksData } from 'redux/actions/clicks.actions';
 import { dispatch_new_links_data } from 'redux/actions/links.actions';
 import Modal from 'react-modal';
+import Select2 from 'react-select';
+
+const deleteClicksOptions = [
+	{
+		label: __('Delete All', 'betterlinks'),
+		value: false,
+	},
+	{
+		label: __('Delete clicks older than 30 days', 'betterlinks'),
+		value: 30,
+	},
+	{
+		label: __('Delete clicks older than 90 days', 'betterlinks'),
+		value: 90,
+	},
+];
 
 const DeleteClicks = ({ fetchCustomClicksData, dispatch_new_links_data, propsForAnalytics }) => {
 	const { customDateFilter } = propsForAnalytics || {};
@@ -13,7 +29,18 @@ const DeleteClicks = ({ fetchCustomClicksData, dispatch_new_links_data, propsFor
 	const [modalIsOpen, setModalIsOpen] = useState(false);
 	const [successfulDeletedItemsCount, setSuccessfulDeletedItemsCount] = useState(0);
 	const [deleteStatus, setDeleteStatus] = useState('idle');
-	const [currentDaysOlderThan, setCurrentDaysOlderThan] = useState(false);
+	const [currentDaysOlderThan, setCurrentDaysOlderThan] = useState(deleteClicksOptions[0]);
+
+	useEffect(() => {
+		if (modalIsOpen) {
+			document?.body?.classList?.add('betterlinks-delete-clicks-modal-popup-opened');
+		} else {
+			document?.body?.classList?.remove('betterlinks-delete-clicks-modal-popup-opened');
+		}
+		return () => {
+			document?.body?.classList?.remove('betterlinks-delete-clicks-modal-popup-opened');
+		};
+	}, [modalIsOpen]);
 
 	const deleteClicksConfirm = (daysOlderThan = false) => {
 		setModalIsOpen(true);
@@ -58,10 +85,21 @@ const DeleteClicks = ({ fetchCustomClicksData, dispatch_new_links_data, propsFor
 		setCurrentDaysOlderThan(false);
 	};
 
-	const handleInitialResetButtonClick = () => {
+	const handleResetButtonClick1 = () => {
 		setModalIsOpen(true);
 		setDeleteStatus('reset_modal_step_1');
 	};
+
+	const handleResetButtonClick2 = () => {
+		setDeleteStatus('reset_modal_step_2');
+	};
+
+	const handleDeleteOptionsChange = (value) => {
+		console.log({ value });
+		setCurrentDaysOlderThan(value);
+	};
+
+	console.log({ deleteStatus });
 
 	return (
 		<>
@@ -90,39 +128,84 @@ const DeleteClicks = ({ fetchCustomClicksData, dispatch_new_links_data, propsFor
 					margin-right: 20px !important;
     				padding: 5px 30px !important;
 				}
+
+				body.betterlinks-delete-clicks-modal-popup-opened button.btl-btn-reset-apply-1{
+					margin: 0 !important;
+					width: 100px;
+				}
+
+				body.betterlinks-delete-clicks-modal-popup-opened .btl-reset-modal-popup-wrapper {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					height: 100%;
+				}
+				body.betterlinks-delete-clicks-modal-popup-opened .btl-reset-modal-popup-step-1{
+					display: flex;
+					margin: 0;
+					padding: 0;
+					gap: 20px;
+				}
+
+				body.betterlinks-delete-clicks-modal-popup-opened .btl-modal-select--full{
+					width: 350px !important;
+				}
+
+				body.betterlinks-delete-clicks-modal-popup-opened .ReactModal__Content{
+					width: 500px;
+					height: 300px;
+					margin: auto;
+					overflow: visible !important;
+    				padding: 60px !important;
+				}
+
+				body.betterlinks-delete-clicks-modal-popup-opened .ReactModal__Overlay {
+					background-color: rgba(0,0,0, 0.5) !important;
+				}
 			`}</style>
-			<div className="btl-analytic-reset-wrapeer">
-				<Modal
-					isOpen={modalIsOpen}
-					onRequestClose={close}
-					// style={modalCustomStyles}
-					ariaHideApp={false}
-				>
-					{deleteStatus === 'reset_modal_step_1' && <h1>Delete step 1</h1>}
-					{deleteStatus === 'deleting' && <h2>Deleting...</h2>}
-					{deleteStatus === 'idle' && (
-						<>
-							<h4>Are You Sure?</h4>
-							<button
-								onClick={() => {
-									handleConfirmDelete();
-									setDeleteStatus('deleting');
-								}}
-							>
-								Yes
-							</button>
-							<button onClick={close}>No</button>
-						</>
-					)}
-					{deleteStatus === 'success' && (
-						<h2>
-							Success!!! <span class="success_delete_count">{successfulDeletedItemsCount}</span> clicks record Deleted!!!
-						</h2>
-					)}
-					{deleteStatus === 'failed' && <h2>Failed!!</h2>}
+			<div className="btl-analytic-reset-wrapeer betterlinks">
+				<Modal isOpen={modalIsOpen} onRequestClose={close} ariaHideApp={false}>
+					<div className="btl-reset-modal-popup-wrapper ">
+						{deleteStatus === 'reset_modal_step_1' && (
+							<div className="btl-reset-modal-popup btl-reset-modal-popup-step-1 betterlinks-body">
+								<Select2
+									className={`btl-modal-select--full `}
+									classNamePrefix="btl-react-select"
+									onChange={handleDeleteOptionsChange}
+									options={deleteClicksOptions}
+									value={currentDaysOlderThan}
+									isMulti={false}
+								/>
+								<button className="button-primary btl-btn-reset-analytics btl-btn-reset-apply-1" onClick={handleResetButtonClick2}>
+									Apply
+								</button>
+							</div>
+						)}
+						{deleteStatus === 'deleting' && <h2>Deleting...</h2>}
+						{deleteStatus === 'idle' && (
+							<>
+								<h4>Are You Sure?</h4>
+								<button
+									onClick={() => {
+										handleConfirmDelete();
+										setDeleteStatus('deleting');
+									}}
+								>
+									Yes
+								</button>
+								<button onClick={close}>No</button>
+							</>
+						)}
+						{deleteStatus === 'success' && (
+							<h2>
+								Success!!! <span class="success_delete_count">{successfulDeletedItemsCount}</span> clicks record Deleted!!!
+							</h2>
+						)}
+						{deleteStatus === 'failed' && <h2>Failed!!</h2>}
+					</div>
 				</Modal>
 
-				<button className="button-primary btl-reset-analytics" onClick={handleInitialResetButtonClick}>
+				<button className="button-primary btl-reset-analytics" onClick={handleResetButtonClick1}>
 					Reset
 				</button>
 
