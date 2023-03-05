3f0d1e4ba8426b7b5ab7b6e089800e1cff5d4e54
hz-tyfoon <md.tifun@gmail.com>
2023-03-05 14:42:18 +0600

    fixed: fetching terms data with rest_api sending 'terms and condition' page in some scenarios

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '4ca66b0d2471ff61133d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '4362a121e626edbfba47');
