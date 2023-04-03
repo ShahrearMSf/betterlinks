2d543fac5d61afa1537077d88989a6ccfc3baa0f
hz-tyfoon <md.tifun@gmail.com>
2023-04-03 15:27:11 +0600

    manage links-> search: renamed placeholder text to include short url & target url

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '183f7095e68b6305167f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '9d3b9a244298c2d61b20');
