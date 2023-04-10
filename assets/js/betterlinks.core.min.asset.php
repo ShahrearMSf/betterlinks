c4d7c5420995b57974258b911c6e4d8e14fde017
hz-tyfoon <md.tifun@gmail.com>
2023-04-10 12:30:26 +0600

    search (list view): improved placeholder text

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '9d3b9a244298c2d61b20');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '90756657d571c8f51de5');
