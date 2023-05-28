45a9c7aa466af8cc0737d5beef6eabf608f949f7
Hridoy Mozumder <hrrarya6@gmail.com>
2023-05-28 09:11:56 +0600

    YARN BUILD

--- assets/js/betterlinks-gutenberg.core.min.asset.php
+++ assets/js/betterlinks-gutenberg.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'af09a076145e5b4de8d2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e476d6aa221e5a13bd37');
--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '10454d72a7fa46947738');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '6558aa3b9bd031b6e32e');
