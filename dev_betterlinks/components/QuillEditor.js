cfd43cb81ebc0fd4eef1c9254657e3b84519cfa1
Hridoy Mozumder <hrrarya6@gmail.com>
2023-08-17 12:23:26 +0600

    added: color & bg color in gutenberg

--- dev_betterlinks/components/QuillEditor.js
+++ dev_betterlinks/components/QuillEditor.js
@@ -1,11 +1,19 @@
 import { __ } from '@wordpress/i18n';
 import ReactQuill from 'react-quill';
 import { edit_gutenberg_affiliate } from 'redux/actions/gutenbergredirectlink.actions';
+import { useEffect } from 'react';
 
 const modules = {
-	toolbar: [['bold', 'italic', 'underline', 'strike', 'blockquote'], [{ header: '1' }, { header: '2' }], [{ list: 'ordered' }, { list: 'bullet' }], ['link', 'image'], ['clean']],
+	toolbar: [
+		['bold', 'italic', 'underline', 'strike', 'blockquote'],
+		[{ color: [] }, { background: [] }],
+		[{ header: '1' }, { header: '2' }],
+		[{ list: 'ordered' }, { list: 'bullet' }],
+		['link', 'image'],
+		['clean'],
+	],
 };
-const formats = ['header', 'font', 'size', 'bold', 'italic', 'underline', 'strike', 'blockquote', 'list', 'bullet', 'indent', 'link', 'image', 'video'];
+// const formats = ['header', 'font', 'size', 'bold', 'italic', 'underline', 'strike', 'blockquote', 'list', 'bullet', 'indent', 'link', 'image', 'video'];
 const QuillEditor = ({ html, setHtml }) => {
 	const theme = 'snow';
 	const handleChange = (html) => {
@@ -15,13 +23,22 @@ const QuillEditor = ({ html, setHtml }) => {
 		});
 	};
 
+	useEffect(() => {
+		const colorPicker = document.querySelectorAll('.ql-picker-label');
+		colorPicker.forEach(function (item) {
+			item.addEventListener('click', function (e) {
+				e.preventDefault();
+			});
+		});
+	}, []);
+
 	return (
 		<span className="btl-form-group btl-general-tab-settings-react-select">
 			<label className="btl-form-label">{__('AFFILIATE LINK TEXT', 'betterlinks-pro')}</label>
 			<div className="btl-form-field">
 				<label className="btl-checkbox-field block">
 					<div className="btl-affiliate-disclosure-editor">
-						<ReactQuill theme={theme} onChange={handleChange} value={html} modules={modules} formats={formats} placeholder={'Type here...'} />
+						<ReactQuill theme={theme} onChange={handleChange} value={html} modules={modules} placeholder={'Type here...'} />
 					</div>
 					{/* <div className="btl-tooltip">
 						<span className="dashicons dashicons-info-outline" />
