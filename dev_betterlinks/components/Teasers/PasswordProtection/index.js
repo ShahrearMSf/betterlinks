8451ad56dca50fa20e739a27e9675301aad375c2
Hridoy Mozumder <hrrarya6@gmail.com>
2023-09-12 11:20:40 +0600

    added: teaser for password protection

---
+++ dev_betterlinks/components/Teasers/PasswordProtection/index.js
@@ -0,0 +1,54 @@
+import UpgradeToPro from '../UpgradeToPro';
+import { __ } from '@wordpress/i18n';
+import { useUpgradeProModal } from 'utils/customHooks';
+import { is_pro_enabled, saveSettingsHandler } from 'utils/helper';
+import { Formik, Form } from 'formik';
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { update_option } from 'redux/actions/settings.actions';
+
+const PasswordProtection = ({ settings }) => {
+	const [formSubmitText, setFormSubmitText] = useState(__('Save Settings', 'betterlinks'));
+	const [isOpenUpgradeToProModal, openUpgradeToProModal, closeUpgradeToProModal] = useUpgradeProModal();
+
+	return (
+		<>
+			<UpgradeToPro isOpenModal={isOpenUpgradeToProModal} closeModal={closeUpgradeToProModal} />
+			<Formik enableReinitialize initialValues={{ ...settings }} onSubmit={(values) => saveSettingsHandler(values, update_option, setFormSubmitText)}>
+				{(props) => (
+					<Form>
+						{!is_pro_enabled && (
+							<span className="btl-form-group btl-form-group--teaser btl-form-group-password-protection" onClick={openUpgradeToProModal}>
+								<label className="btl-form-label">
+									{__('Enable Password Protection', 'betterlinks')} <span className="pro-badge">{__('Pro', 'betterlinks')}</span>
+								</label>
+								<div className="link-options__body">
+									<label className="btl-checkbox-field block">
+										<input className="btl-check" name="enable_password_protection" type="checkbox" disabled={true} />
+										<span className="text" />
+									</label>
+								</div>
+							</span>
+						)}
+						{betterLinksHooks.applyFilters('BetterLinksPasswordProtection', null, props)}
+						{is_pro_enabled && (
+							<>
+								<button className="button-primary btn-save-settings" type="submit">
+									{formSubmitText}
+								</button>
+							</>
+						)}
+					</Form>
+				)}
+			</Formik>
+		</>
+	);
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		update_option: bindActionCreators(update_option, dispatch),
+	};
+};
+export default connect(null, mapDispatchToProps)(PasswordProtection);
