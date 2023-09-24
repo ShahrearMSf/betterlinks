ddc06e88e19cf59cb451ce1daa07e2cb62190459
Hridoy Mozumder <hrrarya6@gmail.com>
2023-09-24 11:43:53 +0600

    update: db version

--- betterlinks.php
+++ betterlinks.php
@@ -81,7 +81,7 @@ if (!class_exists('BetterLinks')) {
              * Defines CONSTANTS for Whole plugins.
              */
             define('BETTERLINKS_VERSION', '1.5.18');
-            define('BETTERLINKS_DB_VERSION', '1.5');
+            define('BETTERLINKS_DB_VERSION', '1.6');
             define('BETTERLINKS_SETTINGS_NAME', 'betterlinks_settings');
             define('BETTERLINKS_PLUGIN_FILE', __FILE__);
             define('BETTERLINKS_PLUGIN_BASENAME', plugin_basename(__FILE__));
