bebb2a64b0bfdbd827a060654b31e8d665053fb2
Hridoy Mozumder <hrrarya6@gmail.com>
2023-10-11 18:37:52 +0600

    YARN BUILD

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '39e4d654c9dfce09d496');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'f6fe2c6cf5b6e077caa3');
