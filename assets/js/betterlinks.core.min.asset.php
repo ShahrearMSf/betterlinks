fef1d6fde348516af205b961a12d51ac87fb12f9
Hridoy Mozumder <hrrarya6@gmail.com>
2024-01-23 12:36:34 +0600

    YARN BUILD
    fixed: console error while refresh analytics - settings page[merged]

--- assets/js/betterlinks.core.min.asset.php
+++ assets/js/betterlinks.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '386cf610e08f958429c8');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '3606cf16eca3c037b639');
