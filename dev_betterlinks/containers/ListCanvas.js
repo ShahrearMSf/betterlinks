40e18b4076246aec840dfe2522e40ce706aaca2f
Hridoy Mozumder <hrrarya6@gmail.com>
2024-02-08 15:36:43 +0600

    adding: target url in the list view

--- dev_betterlinks/containers/ListCanvas.js
+++ dev_betterlinks/containers/ListCanvas.js
@@ -24,8 +24,9 @@ const getLinksListViewColumnData = (props) => {
 			name: __('Title', 'betterlinks'),
 			selector: 'link_title',
 			sortable: false,
-			width: '25%',
+			// width: '25%',
 			cell: (row) => {
+				console.log(row);
 				const expireStatusDot = useBtlExpireStatusDot({ data: row, view: 'list' });
 				return (
 					!!row.link_title && (
@@ -42,20 +43,36 @@ const getLinksListViewColumnData = (props) => {
 			name: __('Shortened URL', 'betterlinks'),
 			selector: 'short_url',
 			sortable: false,
+			// width: '100px',
 			cell: (row) => {
 				return <LinkCopyUrl shortUrl={row.short_url} />;
+			},
+		},
+		{
+			name: __('Target URL', 'betterlinks'),
+			selector: 'target_url',
+			sortable: false,
+			// width: '450px',
+			cell: (row) => {
+				return (
+					<div className="btl-short-url-wrapper">
+						<span className="btl-short-url">{row.target_url}</span>
+					</div>
+				);
 			},
 		},
 		{
 			name: __('Redirect Type', 'betterlinks'),
 			selector: 'redirect_type',
 			sortable: false,
+			width: '150px',
 			cell: (row) => <div>{row.redirect_type == 'cloak' ? 'Cloaked' : row.redirect_type}</div>,
 		},
 		{
 			name: __('Clicks', 'betterlinks'),
 			selector: '',
 			sortable: false,
+			width: '150px',
 			cell: (row) => (
 				<div>
 					{row.analytic ? (
@@ -76,12 +93,14 @@ const getLinksListViewColumnData = (props) => {
 			name: __('Date', 'betterlinks'),
 			selector: 'link_date',
 			sortable: false,
+			width: '150px',
 			cell: (row) => <div>{formatDate(new Date(row.link_date), 'mm/dd/yyyy')}</div>,
 		},
 		{
 			name: __('Action', 'betterlinks'),
 			selector: '',
 			sortable: false,
+			width: '150px',
 			cell: (row) => (
 				<div className="btl-list-view-action-wrapper">
 					<LinkQuickAction
@@ -254,7 +273,7 @@ const ListCanvas = (props) => {
 						subHeaderComponent={subHeaderComponentMemo}
 						persistTableHead
 						selectableRows
-						selectableRowsVisibleOnly
+						// selectableRowsVisibleOnly
 						onSelectedRowsChange={(e) => onSelectedRowsChange(e)}
 						clearSelectedRows={toggledClearRows}
 					/>
