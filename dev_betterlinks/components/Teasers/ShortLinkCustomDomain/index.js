a290bcbc56daee37b57ef5300989ce33a2650fc3
Hridoy Mozumder <hrrarya6@gmail.com>
2024-03-31 09:26:41 +0600

    working on custom domain url

---
+++ dev_betterlinks/components/Teasers/ShortLinkCustomDomain/index.js
@@ -0,0 +1,66 @@
+import { useState, lazy } from 'react';
+import { useUpgradeProModal } from 'utils/customHooks';
+import UpgradeToPro from '../UpgradeToPro';
+import { Form, Formik } from 'formik';
+import { is_pro_enabled, saveSettingsHandler } from 'utils/helper';
+import { update_option } from 'redux/actions/settings.actions';
+import CheckList from '../AutoLinkCreate/CheckList';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { __ } from '@wordpress/i18n';
+import { isURL } from '@wordpress/url';
+
+const ShortLinkCustomDomain = ({ settings, update_option }) => {
+	const [formSubmitText, setFormSubmitText] = useState(__('Save Settings', 'betterlinks'));
+	const [isOpenUpgradeToProModal, openUpgradeToProModal, closeUpgradeToProModal] = useUpgradeProModal();
+	return (
+		<>
+			<UpgradeToPro isOpenModal={isOpenUpgradeToProModal} closeModal={closeUpgradeToProModal} />
+			<Formik
+				enableReinitialize
+				initialValues={{ ...settings }}
+				onSubmit={(values) => {
+					const { enable_shortlink_custom_domain: enabled, shortlink_custom_domain: url } = values;
+					if (enabled && ('' === url || !isURL(url))) {
+						return;
+					}
+					saveSettingsHandler(values, update_option, setFormSubmitText);
+				}}
+			>
+				{(props) => (
+					<Form>
+						{!is_pro_enabled && (
+							<>
+								<CheckList title={__('Enable Custom Domain', 'betterlinks')} onClick={openUpgradeToProModal} />
+								<span className="btl-form-group btl-form-group--top">
+									<label className="btl-form-label">Custom Domain</label>
+									<div className="link-options__body" style={{ flexDirection: 'column' }}>
+										<div style={{ maxWidth: 250 }} onClick={openUpgradeToProModal}>
+											<input className="btl-text-field btl-text-field-teaser" placeholder="Your custom domain here..." disabled />
+										</div>
+									</div>
+								</span>
+							</>
+						)}
+						{betterLinksHooks.applyFilters('BetterLinksCustomDomain', null, props)}
+						{is_pro_enabled && (
+							<>
+								<button className="button-primary btn-save-settings" type="submit">
+									{formSubmitText}
+								</button>
+							</>
+						)}
+					</Form>
+				)}
+			</Formik>
+		</>
+	);
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		update_option: bindActionCreators(update_option, dispatch),
+	};
+};
+
+export default connect(null, mapDispatchToProps)(ShortLinkCustomDomain);
--- dev_betterlinks/containers/TabsOptions.js
+++ dev_betterlinks/containers/TabsOptions.js
@@ -5,6 +5,7 @@ import AutoLinkCreate from 'components/Teasers/AutoLinkCreate';
 import CustomizeMetaTags from 'components/Teasers/CustomizeMetaTags';
 import ExternalAnalytics from 'components/Teasers/ExternalAnalytics';
 import PasswordProtection from 'components/Teasers/PasswordProtection';
+import ShortLinkCustomDomain from 'components/Teasers/ShortLinkCustomDomain';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { is_pro_enabled } from 'utils/helper';
 
@@ -15,6 +16,7 @@ const TabsOptions = ({ settings, autoCreateLinkSettings, terms, trackingSettings
 		__('Affiliate Link Disclosure', 'betterlinks'),
 		__('Password Protected Redirect', 'betterlinks'),
 		__('Customize Link Preview', 'betterlinks'),
+		__('Short Link Custom Domain', 'betterlinks'),
 	];
 	const panelList = [
 		<ExternalAnalytics trackingSettings={trackingSettings} setTrackingSettings={setTrackingSettings} />,
@@ -22,12 +24,13 @@ const TabsOptions = ({ settings, autoCreateLinkSettings, terms, trackingSettings
 		<AffiliateLinkDisclosure settings={settings} />,
 		<PasswordProtection settings={settings} />,
 		<CustomizeMetaTags settings={settings} />,
+		<ShortLinkCustomDomain settings={settings} />,
 	];
 	const optionsTabList = betterLinksHooks.applyFilters('betterLinksSettingsOptionsTabList', tabList);
 	const optionsTabPanelList = betterLinksHooks.applyFilters('betterLinksSettingsOptionsTabPanelList', panelList);
 	return (
 		<div className="betterlinks-options-tabs-wrapper">
-			<Tabs>
+			<Tabs defaultIndex={5}>
 				<TabList>
 					{optionsTabList.map((item, index) => (
 						<Tab key={index}>
