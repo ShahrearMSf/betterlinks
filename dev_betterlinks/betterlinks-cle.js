34e30891bb2246b142ce769055d32c29f2b4e137
Hridoy Mozumder <hrrarya6@gmail.com>
2024-05-14 12:16:21 +0600

    added: falling confetti animation

--- dev_betterlinks/betterlinks-cle.js
+++ dev_betterlinks/betterlinks-cle.js
@@ -14,4 +14,15 @@
 			clearTimeout(timer);
 		}, 2000);
 	});
+
+	// confetti
+	let flakes = '',
+		randomColor;
+	for (let i = 0, len = 400; i < len; i++) {
+		randomColor = Math.floor(Math.random() * 16777215).toString(16);
+		flakes += '<div class="ball" style="background: #' + randomColor;
+		flakes += '; animation-duration: ' + 3 + 's; animation-delay: ';
+		flakes += Math.random() * 2 + 0 + 's;"></div>';
+	}
+	document.getElementById('confetti').innerHTML = flakes;
 })(jQuery);
