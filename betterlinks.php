64f9f650b2ee08deee3eabb0525f84657f2ce3b7
Hridoy Mozumder <hrrarya6@gmail.com>
2024-06-06 14:22:56 +0600

    Version 2.1.0

--- betterlinks.php
+++ betterlinks.php
@@ -3,7 +3,7 @@
  * Plugin Name:		BetterLinks
  * Plugin URI:		https://betterlinks.io/
  * Description:		Ultimate plugin to create, shorten, track and manage any URL. Gather analytics reports and run successfully marketing campaigns easily.
- * Version:			2.0.2
+ * Version:			2.1.0
  * Author:			WPDeveloper
  * Author URI:		https://wpdeveloper.com
  * License:			GPL-3.0+
@@ -80,8 +80,8 @@ if (!class_exists('BetterLinks')) {
             /**
              * Defines CONSTANTS for Whole plugins.
              */
-            define('BETTERLINKS_VERSION', '2.0.3');
-            define('BETTERLINKS_DB_VERSION', '1.6.6');
+            define('BETTERLINKS_VERSION', '2.1.0');
+            define('BETTERLINKS_DB_VERSION', '1.6.4');
             define('BETTERLINKS_MENU_NOTICE', '6');
             define('BETTERLINKS_SETTINGS_NAME', 'betterlinks_settings');
             define('BETTERLINKS_PLUGIN_FILE', __FILE__);
