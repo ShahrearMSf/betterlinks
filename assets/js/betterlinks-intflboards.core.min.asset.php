97ac800ab411406b2685f9f2978b2c359840260b
Hridoy Mozumder <hrrarya6@gmail.com>
2024-06-12 17:22:43 +0600

    YARN BUILD

--- assets/js/betterlinks-intflboards.core.min.asset.php
+++ assets/js/betterlinks-intflboards.core.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a638f1e082fdb63fcbc0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bcd0a6218d9997cfc719');
