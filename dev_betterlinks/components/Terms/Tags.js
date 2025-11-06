2e2fe260f88804c954aba0d01e8ca4a317cf6549
Kamrul Hasan <mdkamrul2058@gmail.com>
2025-11-06 12:00:34 +0600

    Fixed Tags Field In Popup Loading Issue

--- dev_betterlinks/components/Terms/Tags.js
+++ dev_betterlinks/components/Terms/Tags.js
@@ -13,10 +13,12 @@ const propTypes = {
 };
 
 const Tags = ({ fieldName, linkId, setFieldValue, data, disabled }) => {
-	const [saveTags, setSaveTags] = useState(null);
+	const [selectedTags, setSelectedTags] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		if (linkId) {
+			setIsLoading(true);
 			let form_data = new FormData();
 			form_data.append('action', 'betterlinks/admin/get_terms');
 			form_data.append('security', betterlinks_nonce);
@@ -25,25 +27,31 @@ const Tags = ({ fieldName, linkId, setFieldValue, data, disabled }) => {
 			axios.post(ajaxurl, form_data).then(
 				(res) => {
 					if (res.data.data) {
-						setSaveTags(
-							res.data.data.map((item) => ({
-								value: item.term_id,
-								label: item.term_name,
-							}))
-						);
+						const tags = res.data.data.map((item) => ({
+							value: item.term_id,
+							label: item.term_name,
+						}));
+						setSelectedTags(tags);
+						// Also set the initial Formik value
+						setFieldValue(fieldName, tags.map((item) => item.value));
 					}
+					setIsLoading(false);
 				},
 				(error) => {
 					console.log(error);
+					setIsLoading(false);
 				}
 			);
-		} else {
-			setSaveTags([]);
 		}
-	}, []);
+	}, [linkId]);
+	
 	const [field] = useField(fieldName);
 	const onChange = (option) => {
-		if (option == null) {
+		// Update local state for UI
+		setSelectedTags(option || []);
+		
+		// Update Formik state for form submission
+		if (option == null || option.length === 0) {
 			return setFieldValue(field.name, '');
 		}
 
@@ -54,29 +62,29 @@ const Tags = ({ fieldName, linkId, setFieldValue, data, disabled }) => {
 	};
 	return (
 		<React.Fragment>
-			{saveTags && (
-				<CreatableSelect2
-					className="btl-modal-form-control btl-modal-select"
-					isClearable
-					id={field.id}
-					name={field.name}
-					defaultValue={saveTags}
-					onChange={onChange}
-					classNamePrefix="btl-react-select"
-					options={
-						data.terms &&
-						data.terms
-							.filter((item) => item.term_type == 'tags')
-							.sort((a, b) => a.term_name.localeCompare(b.term_name))
-							.map((item) => ({
-								value: item.ID,
-								label: item.term_name,
-							}))
-					}
-					isDisabled={disabled}
-					isMulti={true}
-				/>
-			)}
+			<CreatableSelect2
+				className="btl-modal-form-control btl-modal-select"
+				isClearable
+				id={field.id}
+				name={field.name}
+				value={selectedTags}
+				onChange={onChange}
+				classNamePrefix="btl-react-select"
+				options={
+					data.terms &&
+					data.terms
+						.filter((item) => item.term_type == 'tags')
+						.sort((a, b) => a.term_name.localeCompare(b.term_name))
+						.map((item) => ({
+							value: item.ID,
+							label: item.term_name,
+						}))
+				}
+				isDisabled={disabled || isLoading}
+				isLoading={isLoading}
+				isMulti={true}
+				placeholder={isLoading ? 'Loading tags...' : 'Select or create tags...'}
+			/>
 		</React.Fragment>
 	);
 };
